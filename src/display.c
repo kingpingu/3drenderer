2326f8ffd33a5a2ec9e9e74300553df84a476a15
kingpingu <kylerjgriffith@gmail.com>
2020-07-04 12:42:57 -0700

    added perspective projection

--- src/display.c
+++ src/display.c
@@ -45,8 +45,14 @@ bool initialize_window(void) {
 void draw_grid(void) {
 	for (int y = 0; y < window_height; y += 10) {
 		for (int x = 0; x < window_width; x += 10) {
-			color_buffer[(window_width * y) + x] = 0xFF444444;
+			draw_pixel(x, y, 0xFF444444);
 		}
+	}
+}
+
+void draw_pixel(int x, int y, uint32_t color) {
+	if (x >= 0 && x < window_width && y >= 0 && y < window_height) {
+		color_buffer[(window_width * y) + x] = color;
 	}
 }
 
@@ -55,7 +61,7 @@ void draw_rect(int x, int y, int width, int height, uint32_t color) {
 		for (int j = 0; j < height; j++) {
 			int current_x = x + i;
 			int current_y = y + j;
-			color_buffer[(window_width * current_y) + current_x] = color;
+			draw_pixel(current_x, current_y, color);
 		}
 	}
 }
--- src/main.c
+++ src/main.c
@@ -3,6 +3,16 @@
 #include <stdint.h>
 #include <SDL2/SDL.h>
 #include "display.h"
+#include "vector.h"
+
+#define N_POINTS (9 * 9 * 9)
+
+vec3_t cube_points[N_POINTS];
+vec2_t projected_points[N_POINTS];
+
+vec3_t camera_position = { .x = 0, .y = 0, .z = -5 };
+
+float fov_factor = 640;
 
 bool is_running = false;
 
@@ -16,6 +26,17 @@ void setup(void) {
 		window_width,
 		window_height
 	);
+
+	int point_count = 0;
+	
+	for (float x = -1; x <= 1; x += 0.25) {
+		for (float y = -1; y <= 1; y += 0.25) {
+			for (float z = -1; z <= 1; z += 0.25) {
+				vec3_t new_point = { .x = x, .y = y, .z = z };
+				cube_points[point_count++] = new_point;
+			}	
+		}
+	}
 }
 
 void process_input(void) {
@@ -34,19 +55,43 @@ void process_input(void) {
 	}
 }
 
-void update(void) {
+vec2_t project(vec3_t point) {
+	vec2_t projected_point = {
+		.x = (fov_factor * point.x) / point.z,
+		.y = (fov_factor * point.y) / point.z
+	};
 
+	return projected_point;
+}
+
+void update(void) {
+	for (int i = 0; i < N_POINTS; i++) {
+		vec3_t point = cube_points[i];
+
+		point.z -= camera_position.z;
+
+		vec2_t projected_point = project(point);
+
+		projected_points[i] = projected_point;
+	}
 }
 
 void render(void) {
-	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-	SDL_RenderClear(renderer);
-
 	draw_grid();
-	draw_rect(300, 200, 300, 150, 0xFFFF00FF);
+
+	for (int i = 0; i < N_POINTS; i++) {
+		vec2_t projected_point = projected_points[i];
+		draw_rect(
+			projected_point.x + window_width / 2, 
+			projected_point.y + window_height / 2, 
+			4, 
+			4, 
+			0xFFFFFF00
+		);
+	}
 
 	render_color_buffer();
-	clear_color_buffer(0xFFFFFF00);
+	clear_color_buffer(0xFF000000);
 
 	SDL_RenderPresent(renderer);
 }
