892f8d7f15cb2bfeb8bdc5b039d4a313e45a39fd
kingpingu <kylerjgriffith@gmail.com>
2020-07-03 20:46:27 -0700

    add header files

--- src/main.c
+++ src/main.c
@@ -1,6 +1,68 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <SDL2/SDL.h>
+#include "display.h"
+
+bool is_running = false;
+
+void setup(void) {
+	color_buffer = (uint32_t*) malloc(sizeof(uint32_t) * window_width * window_height);
+
+	color_buffer_texture = SDL_CreateTexture(
+		renderer,
+		SDL_PIXELFORMAT_ARGB8888,
+		SDL_TEXTUREACCESS_STREAMING,
+		window_width,
+		window_height
+	);
+}
+
+void process_input(void) {
+	SDL_Event event;
+	SDL_PollEvent(&event);
+
+	switch (event.type) {
+		case SDL_QUIT:
+			is_running = false;
+			break;
+		case SDL_KEYDOWN:
+			if (event.key.keysym.sym == SDLK_ESCAPE) {
+				is_running = false;
+			}
+			break;
+	}
+}
+
+void update(void) {
+
+}
+
+void render(void) {
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+	SDL_RenderClear(renderer);
+
+	draw_grid();
+	draw_rect(300, 200, 300, 150, 0xFFFF00FF);
+
+	render_color_buffer();
+	clear_color_buffer(0xFFFFFF00);
+
+	SDL_RenderPresent(renderer);
+}
 
 int main(void) {
-	printf("HELLO, WORLD!\n");
+	is_running = initialize_window();
+
+	setup();
+
+	while(is_running) {
+		process_input();
+		update();
+		render();
+	}
+
+	destroy_window();
+
 	return 0;
 }
